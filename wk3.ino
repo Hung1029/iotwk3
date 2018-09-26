71330cd9de171edc4ab23ff1a8f7b9dccdede6cc
Hung1029 <39110096+Hung1029@users.noreply.github.com>
2018-09-26 11:41:42 +0800

    Add files via upload

---
+++ wk3.ino
@@ -0,0 +1,22 @@
+#include <Wire.h> 
+#include "SSD1306Wire.h" 
+SSD1306Wire  display(0x3c, 21, 22);
+
+
+void setup() {
+ 
+  display.init();
+  display.flipScreenVertically();
+  display.setFont(ArialMT_Plain_24);
+  
+
+}
+
+void loop() {
+display.clear();
+display.drawString(40,20, String(analogRead(A0)));
+display.display();
+delay(200);
+
+
+}
